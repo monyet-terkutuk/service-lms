e9e7effe374666d824961c2a4791d90bce594eed
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-10-21 01:52:44 +0700

    sip

---
+++ migrations/20231020184913-create-table-courses.js
@@ -0,0 +1,60 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("courses", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      nim: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      division: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.ENUM,
+        values: ["admin", "mentor", "student"],
+        defaultValue: "student",
+      },
+      number_phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image_profile: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("courses");
+  },
+};
