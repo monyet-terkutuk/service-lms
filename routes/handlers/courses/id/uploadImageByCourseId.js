17314658bcd17e32d5ea2cd44aaf630f60272a52
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-11-09 23:00:30 +0700

    gambar

--- routes/handlers/courses/id/uploadImageByCourseId.js
+++ routes/handlers/courses/id/uploadImageByCourseId.js
@@ -1,4 +1,14 @@
 const { Course } = require("../../../../models");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+require("dotenv").config();
+
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
 
 module.exports = async (req, res) => {
   const { courseId } = req.params;
@@ -16,9 +26,23 @@ module.exports = async (req, res) => {
       data: "Course not found",
     });
 
-  body.image_course = `images/courses/${req.file.filename}`;
-
   try {
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME, // Ganti dengan variabel lingkungan nama bucket AWS S3 kamu
+      Key: `images/courses/${req.file.filename}`,
+      Body: req.file.buffer,
+      ACL: "public-read",
+    };
+    console.error();
+
+    // Mengunggah file ke AWS S3 menggunakan AWS SDK for JavaScript versi 3
+    await s3Client.send(new PutObjectCommand(params));
+    console.error();
+
+    // Update database dengan URL gambar di AWS S3
+    body.image_course = `https://${process.env.AWS_S3_BUCKET_NAME}.s3-${process.env.AWS_REGION}.amazonaws.com/${params.Key}`;
+
+    console.error();
     await course.update({ ...body });
 
     return res.json({
@@ -27,16 +51,18 @@ module.exports = async (req, res) => {
         code: 200,
         status: "success",
       },
-      data: course.image_course,
+      data: body.image_course,
     });
   } catch (error) {
+    console.error(error);
+
     return res.status(500).json({
       meta: {
         message: "Internal server error",
         code: 500,
         status: "error",
       },
-      data: "Error uploade image course",
+      data: "Error uploading image course",
     });
   }
 };
